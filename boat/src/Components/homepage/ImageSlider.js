39bb2d200dc3f9324cd29686c0c56a8d739f1162
soumyah1512 <99666938+soumyah1512@users.noreply.github.com>
2022-05-05 19:09:48 +0530

    img added

--- boat/src/Components/homepage/ImageSlider.js
+++ boat/src/Components/homepage/ImageSlider.js
@@ -1,12 +1,13 @@
 import SimpleImageSlider from "react-simple-image-slider";
 import img1 from "../../assets/slider-img-1.png";
+import imgx from "../../assets/img-temp.webp"
 import img2 from "../../assets/slider-img-2.png";
 import img3 from "../../assets/slider-img-3.png";
 import img4 from "../../assets/slider-img-4.png";
 import img5 from "../../assets/slider-img-5.png";
 
 const images = [
-    { url: img1 },
+    { url: imgx },
     { url: img2 },
     { url: img3 },
     { url: img4 },
