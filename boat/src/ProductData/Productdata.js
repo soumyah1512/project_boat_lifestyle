3d1648d129f6562573df4a8ae3198542c1e840ed
rahulyo954030 <100074739+rahulyo954030@users.noreply.github.com>
2022-05-05 12:44:46 +0530

    data page

---
+++ boat/src/ProductData/Productdata.js
@@ -0,0 +1,67 @@
+const ProductsArray =[
+    { id : 1,
+      image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/32011675-2ad8-4b99-9787-895caf201d28_300x.png?v=1642405569",
+      title: "boAt Airdopes 131",
+      price: 1099 ,
+      mrp:  2990,
+      color: "6 colors available"
+  },
+  { id : 2,
+    image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/32011675-2ad8-4b99-9787-895caf201d28_300x.png?v=1642405569",
+      title: "boAt Airdopes 131",
+      price: 1099 ,
+      mrp:  2990,
+      color: "6 colors available"
+  },
+  { id : 3,
+    image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/32011675-2ad8-4b99-9787-895caf201d28_300x.png?v=1642405569",
+      title: "boAt Airdopes 131",
+      price: 1099 ,
+      mrp:  2990,
+      color: "6 colors available"
+  },
+  {id : 4,
+    image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/32011675-2ad8-4b99-9787-895caf201d28_300x.png?v=1642405569",
+      title: "boAt Airdopes 131",
+      price: 1099 ,
+      mrp:  2990,
+      color: "6 colors available"
+  },
+  { id : 5,
+    image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/32011675-2ad8-4b99-9787-895caf201d28_300x.png?v=1642405569",
+      title: "boAt Airdopes 131",
+      price: 1099 ,
+      mrp:  2990,
+      color: "6 colors available"
+  },
+  {id : 6,
+    image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/32011675-2ad8-4b99-9787-895caf201d28_300x.png?v=1642405569",
+      title: "boAt Airdopes 131",
+      price: 1099 ,
+      mrp:  2990,
+      color: "6 colors available"
+  },
+  {id : 7,
+    image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/32011675-2ad8-4b99-9787-895caf201d28_300x.png?v=1642405569",
+      title: "boAt Airdopes 131",
+      price: 1099 ,
+      mrp:  2990,
+      color: "6 colors available"
+  },
+  {id : 8,
+    image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/32011675-2ad8-4b99-9787-895caf201d28_300x.png?v=1642405569",
+      title: "boAt Airdopes 131",
+      price: 1099 ,
+      mrp:  2990,
+      color: "6 colors available"
+  },
+  {id: 9,
+    image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/32011675-2ad8-4b99-9787-895caf201d28_300x.png?v=1642405569",
+      title: "boAt Airdopes 131",
+      price: 1099 ,
+      mrp:  2990,
+      color: "6 colors available"
+  },
+  ]
+
+  export default ProductsArray
