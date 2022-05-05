fd81caf3fbf3c8c724450241e643a8254f6d0bf9
tarunaNangla <tarunanangla131@gmail.com>
2022-05-05 13:00:13 +0530

    footer
    footer work

--- boat/src/App.js
+++ boat/src/App.js
@@ -2,12 +2,14 @@
 import { Navbar } from '../src/Components/Navbar/Navbar';
 import { HomePage } from './Components/homepage/HomePage';
 // import { Rout } from '../src/routes/Routes';
+import {Footer} from './Components/Footer'
 
 function App() {
   return (<>
         <Navbar/>
         <HomePage/>
         {/* <h1>sdfsa</h1> */}
+        <Footer />
       </>)
 
 
